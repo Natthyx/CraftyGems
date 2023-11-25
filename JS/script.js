0923522c26510edef2679fbd6af832be9d30cb1f
Natthyx <natannanz16@gmail.com>
2023-11-25 15:34:22 +0300

    add product feature in landingpage

--- JS/script.js
+++ JS/script.js
@@ -22,6 +22,7 @@ function myFunction() {
     header.classList.remove("sticky");
   }
 }
+//swiper
 const swiper = new Swiper('.swiper', {
   // Optional parameters
   // direction: 'vertical',
@@ -43,5 +44,32 @@ const swiper = new Swiper('.swiper', {
     el: '.swiper-scrollbar',
   },
 });
+
+const productList=[
+  {id:1,image:'../images/goldNecklace.jpg',productName:"Gold Necklace",price:"14000 Birr"},
+  {id:2,image:'../images/earing.jpg',productName:"Earing",price:"1000 Birr"},
+  {id:3,image:'../images/magneticbracelet.jpeg',productName:"Magnetic Bracelet",price:"2000 Birr"},
+  {id:4,image:'../images/Lara.jpg',productName:"Lara Earing",price:"3000 Birr"},
+];
+
+const productListContainer = document.getElementById('row');
+
+  // Map over the products array and create HTML elements
+  const productElements = productList.map(product => {
+    const productDiv = document.createElement('div');
+
+    productDiv.classList.add("productCol")
+    productDiv.innerHTML = `
+      <img src="${product.image}" alt="${product.productName}">
+      <h4>${product.productName}</h4>
+      <h5>${product.price}</h5>
+      <button class="buybtn">BUY NOW</button>
+    `;
+    return productDiv;
+  });
+  // Append the product elements to the container
+  productElements.forEach(productElement => {
+    productListContainer.appendChild(productElement);
+  });
 
 
---
+++ constant.js
@@ -0,0 +1,12 @@
+export const Product =[
+{
+    Id:0,
+    image:'./images/bracelet.jpg',
+
+},
+{
+    Id:1,
+    image:'./images/bracelet.jpg',
+
+}
+]
