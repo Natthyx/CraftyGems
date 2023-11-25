ae1d28ccae994d3a127bcab47ddf2ac72db0646d
Natthyx <natannanz16@gmail.com>
2023-11-25 11:24:58 +0300

    modify landingpage

--- JS/script.js
+++ JS/script.js
@@ -22,4 +22,26 @@ function myFunction() {
     header.classList.remove("sticky");
   }
 }
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  // direction: 'vertical',
+  // loop: true,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: '.swiper-scrollbar',
+  },
+});
+
 
